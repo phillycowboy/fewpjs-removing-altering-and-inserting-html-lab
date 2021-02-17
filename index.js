c0cb65d3b7a902af23f9f456e8d1593a4a0168a1
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-17 14:48:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Ben is the champion"
